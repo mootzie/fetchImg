0ec9ab9555529ffd746ea8f331f3ce8f1be4fce1
mootzie <tylerw77156@gmail.com>
2022-09-24 23:42:45 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function fetchPics(){
+    let catsImgDiv = document.querySelector(".catsImgDiv")
+    catsImgDiv.innerHTML='';
+    fetch("https://api.thecatapi.com/v1/images/search") .then(
+    (response)=>
+     response.json()
+    )
+    .then ((data) => {
+        let catsImgUrl=data[0].url;
+        let catImgEl = document.createElement("img")
+        catImgEl.setAttribute('src',`${catsImgUrl}`)
+        catImgEl.classList.add("showcase")
+        let catsImgDiv = document.querySelector(".catsImgDiv")
+        catsImgDiv.appendChild(catImgEl);
+    })
+    .catch (err=>console.log(err))
+}
+fetchPics();
